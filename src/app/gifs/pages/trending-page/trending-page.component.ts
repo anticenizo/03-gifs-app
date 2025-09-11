d0e5e58acadd55795712b57e4b6e8f21422a0e9c
Raul del Hoyo Padilla <raul.delhoyo@trijazac.gob.mx>
2025-09-11 14:23:07 -0600

    trending

---
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-trending-page',
+  imports: [RouterOutlet],
+  templateUrl: './trending-page.component.html',
+})
+export default class TrendingPageComponent { }
