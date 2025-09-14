9b1ceb2bcfc540b3df1cc954aa3204180919a518
Raul del Hoyo Padilla <raul.delhoyo@trijazac.gob.mx>
2025-09-14 16:54:45 -0600

    siguiente seccion

--- src/app/gifs/components/gift-list/gift-list.component.ts
+++ src/app/gifs/components/gift-list/gift-list.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { GiftListItemComponent } from './gift-list-item/gift-list-item.component';
+
+
+
 
 @Component({
   selector: 'gift-list',
   imports: [GiftListItemComponent],
   templateUrl: './gift-list.component.html',
 })
-export class GiftListComponent { }
+export class GiftListComponent{
+
+  gifs = input.required<string[]>();
+
+}
